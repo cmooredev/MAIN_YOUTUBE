1b507ae13d5fce6f9a48973239942852532abb5a
cmooredev <46297716+cmooredev@users.noreply.github.com>
2022-10-05 17:59:24 -0700

    added oauth

--- oauth/public/login.js
+++ oauth/public/login.js
@@ -13,8 +13,34 @@ window.onload = () => {
   })
     .then(result => result.json())
     .then(response => {
-      const { username, discriminator } = response;
-      document.getElementById('info').innerText = `Hello, ${username}#${discriminator}\n`;
+      const { username, discriminator, id, avatar } = response;
+      document.getElementById('info').innerText = `Hello, ${username}#${discriminator}`;
+      /**
+      ** Load image from id and avatar(hash)
+      **/
+      let img = document.createElement('img');
+      let src = `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`;
+      img.src = src;
+      document.body.appendChild(img);
     })
     .catch(console.error);
+
+    fetch('https://discord.com/api/users/@me/guilds', {
+      headers: {
+        authorization: `${tokenType} ${accessToken}`,
+      },
+    })
+      .then(result => result.json())
+      .then(response => {
+        let guilds = document.createElement('div');
+        guilds.innerText = 'GUILDS';
+        document.body.appendChild(guilds);
+        for(const guild of response){
+          let g = document.createElement('p');
+          g.innerText += `${guild.name}`;
+          guilds.appendChild(g);
+        }
+
+      })
+      .catch(console.error);
 };
